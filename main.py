4ccdd24dffd3b7e1623770a98ecc0b0d509a9f4a
Daniel Leppänen <daniel.leppanen@gmail.com>
2020-10-11 19:05:01 +0200

    Move pickle data management into separate module. Update to dataframes from start

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 
-import pickle
-from pprint import pprint
-
-from modules.printers import print_comment, print_subreddits_overview, print_comments, progress_bar
-from modules.reddit import get_subreddits
 from modules.tfidf_custom import TfIdfCustom
 from modules.tfidf import TfIdf
 from modules.word_stats import WordStats
 from modules.classifiers.knn import Knn
-from scipy.stats import binned_statistic
+from modules.data import fetch_fresh, load_pickle, save_pickle, create_df_from_reddit_data
+# from scipy.stats import binned_statistic
 import sys
 import argparse
 import pandas as pd
@@ -40,101 +36,48 @@ default_subreddits = [
 
 parser = argparse.ArgumentParser(prog="python3.8 main.py", description=description, epilog=epilog)
 parser.add_argument("--fetch-fresh", dest="use_reddit", action="store_true", default=False, help="Use to fetch new comments")
-parser.add_argument("--save-pickle", dest="save_pickle", action="store_true", default=False, help="Use to save comment data to a pickle")
-parser.add_argument("--load-pickle", dest="load_pickle", action="store_true", default=False, help="Use to load comment data to a pickle")
+parser.add_argument("--save-pickle", dest="save_pickle", nargs="?", type=str, help="Use to save comment data to a pickle")
+parser.add_argument("--load-pickle", dest="load_pickle", nargs="?", type=str, help="Use to load comment data to a pickle")
 parser.add_argument("--subreddits",  dest="subreddits", nargs="+", default=default_subreddits, help="Which subreddits to parse for commments")
 parser.add_argument("--submissions", dest="submissions", nargs="?", default=100, type=int, help="How many submissions to fetch for each subreddit")
 arguments = vars(parser.parse_args())
 
+arg_fetch_fresh = arguments.get("fetch_fresh", False)
+arg_load_pickle = arguments.get("load_pickle", "comments.p")
+arg_save_pickle = arguments.get("save_pickle", "comments.p")
+arg_number_of_submissions = arguments.get("submissions", 100)
+arg_subreddits = arguments.get("subreddits", default_subreddits)
 
-fetch_fresh = arguments.get("use_reddit", False)
-load_picle = arguments.get("load_pickle", False)
-save_pickle = arguments.get("save_pickle", False)
-number_of_submissions = arguments.get("submissions", 100)
-subreddits = arguments.get("subreddits", default_subreddits)
-
-data = []
-
-# data = np.random.rand(1000)
-# bins = binned_statistic(data, data, bins=3, range=[0,10])
-# print(bins)
-# print(bins[0])
-# exit("Exited.")
-
-
-if load_picle:
-    data = pickle.load(open("comments.p", "rb"))
+if arg_load_pickle:
+    df = load_pickle(arg_load_pickle)
+    # df = create_df_from_reddit_data(data)
+    sys.exit("Pickle loaded.")
     print("Pickle loaded.")
 
-if fetch_fresh:
-    data = get_subreddits(subreddits, number_of_submissions)
+elif arg_fetch_fresh:
+    df = fetch_fresh(arg_subreddits, arg_number_of_submissions)
+    print("Data fetched from subreddits.")
 
-if save_pickle:
-    pickle.dump(data, open("comments.p", "wb"))
-    print_subreddits_overview(data, only_outlier_comments=False)
-    sys.exit("Pickle saved.")
-
-if len(data) == 0:
+try:
+    df = df
+except NameError:
     sys.exit("No data loaded.")
 
+if arg_save_pickle:
+    save_pickle(df, arg_save_pickle)
+    sys.exit("Pickle saved.")
 
-# print_subreddits_overview(data, only_outlier_comments=False)
-# exit("Exited.")
 
+# ANALYSIS:
 
-# re-order do message-document format
-# document = several (messagag =
-#   several (sentence =
-#       several (word =
-#           several character)))
-document = []
-for subreddit in data:
-    for submission in subreddit.submissions:
-        for comment in submission.comments:
-            document.append({
-                "body": comment.body,
-                "score": comment.score,
-                "subreddit": subreddit.display_name,
-                "submission": submission.title,
-                "features": {
-                    "tf_matrix": [],
-                    "idf_matrix": [],
-                    "tfidf_score": 0,
-                    "tfidf_custom_score": 0,
-                    "length": 0,
-                    "sentences": 0,
-                    "common_word": 0,
-                    "words_count": 0,
-                    "stop_words_count": 0,
-                    "bad_words_count": 0,
-                    "bad_words": "",
-                    "letter_count": 0,
-                    # "swear_words": False, --- called bad_words
-                    "emoticons": False,
-                    "aspectual_class": 0,
-                    "reichenbach_tense": 0,
-                    "topic_similarity": 0,
-                }
-            })
-
-scores = [item["score"] for item in document]
+# scores = [item["score"] for item in document]
 # DON'T bin labels on regression
 # print(scores)
 # bins = binned_statistic(score, score, bins=2, range=[0,1])
 # bins = binned_statistic(scores, scores, range=[-10, 0,100,1000])
 
 
-df = pd.DataFrame(data={
-    "body": [item["body"] for item in document],
-    "tfidf_custom_score": [item["features"]["tfidf_custom_score"] for item in document],
-    "words_count": [item["features"]["words_count"] for item in document],
-    "stop_words_count": [item["features"]["stop_words_count"] for item in document],
-    "bad_words_count": [item["features"]["bad_words_count"] for item in document],
-    "bad_words": [item["features"]["bad_words"] for item in document],
-    "score": scores,
-})
-
-# TODO: add another pickle here so we do not need to transform every time
+# FEATURES:
 
 word_stats = WordStats()
 df = word_stats.measure(df)
@@ -142,9 +85,18 @@ df = word_stats.measure(df)
 tfidf_custom = TfIdfCustom()
 df = tfidf_custom.measure(df)
 
-print(df)
+# tfidf = TfIdf()
+# tfidf.measure(document)
 
-# exit("Exited.")
+print(df[[
+    "tfidf_custom_score",
+    "words_count"
+    "stop_words_count"
+    "bad_words_count"
+    "bad_words"
+]])
+
+exit("Exited.")
 
 feature_list_to_classify = [
     "words_count",
@@ -155,14 +107,3 @@ feature_list_to_classify = [
 
 knn = Knn(df=df, split=0.9, feature_list=feature_list_to_classify, n_neighbors_max=26)
 print(knn)
-
-
-# Mark with features
-
-# print(data[1].submissions[2].comments[4].body)
-
-
-
-# # tfidf = TfIdf()
-# # tfidf.measure(document)
-# pprint(document)
---
+++ modules/data.py
@@ -0,0 +1,89 @@
+import os
+import sys
+from modules.reddit import get_subreddits
+from modules.printers import print_subreddits_overview
+import pandas as pd
+import pickle
+
+def fetch_fresh(subreddits, number_of_submissions):
+    reddit_data = get_subreddits(subreddits, number_of_submissions)
+    df = create_df_from_reddit_data(reddit_data)
+    return df
+
+def load_pickle(pickle_file_name):
+    if not os.path.isfile(pickle_file_name):
+        sys.exit("Pickle file not found")
+    df = pickle.load(open(pickle_file_name, "rb"))
+    return df
+
+def save_pickle(data, pickle_file_name):
+    pickle.dump(data, open(pickle_file_name, "wb"))
+    # print_subreddits_overview(data, only_outlier_comments=False)
+
+def create_df_from_reddit_data(reddit_data):
+    document = {}
+    i = 0
+    for subreddit in reddit_data:
+        for submission in subreddit.submissions:
+            for comment in submission.comments:
+                document[i] = {
+                    "body": comment.body,
+                    "subreddit": subreddit.display_name,
+                    "submission": submission.title,
+                    # "tf_matrix": [],
+                    # "idf_matrix": [],
+                    "tfidf_score": 0,
+                    "tfidf_custom_score": 0,
+                    "length": 0,
+                    "sentences": 0,
+                    "common_word": 0,
+                    "words_count": 0,
+                    "stop_words_count": 0,
+                    "bad_words_count": 0,
+                    "bad_words": "",
+                    "letter_count": 0,
+                    "emoticons": False,
+                    "aspectual_class": 0,
+                    "reichenbach_tense": 0,
+                    "topic_similarity": 0,
+                    "label": comment.score,
+                }
+                i = i+1
+    return pd.DataFrame.from_dict(document, "index")
+
+# It is very slow to append to df for some reason:
+# https://stackoverflow.com/questions/27929472/improve-row-append-performance-on-pandas-dataframes
+# def create_df_from_data_slow_append(data):
+#     df = pd.DataFrame(columns=[
+#         "tf_matrix",
+#         "idf_matrix",
+#         "tfidf_score",
+#         "tfidf_custom_score",
+#         "length",
+#         "sentences",
+#         "common_word",
+#         "words_count",
+#         "stop_words_count",
+#         "bad_words_count",
+#         "bad_words",
+#         "letter_count",
+#         "emoticons",
+#         "aspectual_class",
+#         "reichenbach_tense",
+#         "topic_similarity",
+#     ])
+#     for subreddit in data:
+#         for submission in subreddit.submissions:
+#             for comment in submission.comments:
+#                 df.append({
+#                     "body": comment.body,
+#                     "subreddit": subreddit.display_name,
+#                     "submission": submission.title,
+#                     "tfidf_custom_score": 0,
+#                     "words_count": 0,
+#                     "stop_words_count": 0,
+#                     "bad_words_count": 0,
+#                     "bad_words": "",
+#                     "score": comment.score,
+#                 }, ignore_index=True)
+#     return df
