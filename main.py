174cafc2b0ebba26cd86bd7e8fde79c0bfdd5798
Daniel Leppänen <daniel.leppanen@gmail.com>
2020-11-10 21:42:41 +0100

    Update main.py to use for reddit and pickle functionality only

--- main.py
+++ main.py
@@ -1,17 +1,6 @@
-from modules.bag_of_words import BagOfWords
-from modules.bigram_of_words import BigramOfWords
-import matplotlib.pyplot as plt
-from modules.tfidf_custom import TfIdfCustom
-from modules.tfidf import TfIdf
-from modules.word_stats import WordStats
-from modules.similarity import Similarity
-from models.knnreg import KnnReg
 from modules.data import fetch_fresh, load_pickle, save_pickle
-from modules.stats import score_distribution_plot, words_count_plot, tfidf_custom_score_plot, topic_similarity_score_plot
-# from scipy.stats import binned_statistic
 import sys
 import argparse
-## Command line usage and arg parsing
 
 description="""
 This programs needs data from live reddit or stored pickle. Therefore, one of
@@ -20,7 +9,7 @@ If --fetch-fresh is supplied it would be good to --save-pickle as well.
 """
 
 epilog="""
-Written in october 2020 for a private project in linguistics at Göteborg Universitet.
+Written in oct 2020 for a private project in linguistics at Göteborg Universitet.
 By Daniel Leppänen.
 """
 
@@ -62,45 +51,3 @@ except NameError:
 if arg_save_pickle:
     save_pickle(df, arg_save_pickle)
     sys.exit("Pickle saved.")
-
-
-
-
-
-# FEATURES:
-
-#bigram_of_words = BigramOfWords()
-#df = bigram_of_words.score(df)
-
-# bag_of_words = BagOfWords()
-# df = bag_of_words.score(df)
-
-#similarity = Similarity()
-#similarity.score(df) # "topic_similarity"
-
-#word_stats = WordStats()
-#df = word_stats.score(df) # "words_count", "stop_words_count", "bad_words_count", "bad_words"
-
-#tfidf_custom = TfIdfCustom()
-#df = tfidf_custom.score(df) # "tfidf_custom_score"
-
-# tfidf = TfIdf()
-# tfidf.score(df) # "tf_idf"
-
-df = df.drop([
-    "topic_similarity", "words_count",
-    "bigram_of_words", "tfidf_score",
-    "stop_words_count", "bad_words_count", "bad_words",
-    "reichenbach_tense", "emoticons", "aspectual_class", "common_word", "letter_count", "tfidf_custom_score", "length", "sentences"
-], axis="columns")
-print(df)
-
-
-
-# ANALYSIS:
-
-# score_distribution_plot(df)
-# words_count_plot(df)
-# tfidf_custom_score_plot(df)
-# topic_similarity_score_plot(df)
-# plt.show()
